631eeea2013c1d1039c583ffdb12e23cd7cd9b8e
Khondwani Sikasote <Khondwani66@gmail.com>
2022-04-24 18:33:55 +0200

    Initial Commit

---
+++ ScoreIT/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ScoreIT
+//
+//  Created by Khondwani Sikasote on 2022/04/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
